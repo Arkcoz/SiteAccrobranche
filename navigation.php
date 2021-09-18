e5c2fcf6e9625ecc141e19b140dcf5b22ff0c9c8
Arkcoz <lucas.mnt21@gmail.com>
2021-09-18 20:05:54 +0200

    ajout de classes
    ajout de classes

---
+++ navigation.php
@@ -0,0 +1,42 @@
+<nav class="navbar">
+
+    
+
+    <ul class="navbar-menu">
+        <a href="parcours.php" class="navbar-link">
+            <li classe="navbar-item">
+                Parcours
+            </li>
+        </a>
+        <a href="reservation.php" class="navbar-link">
+            <li classe="navbar-item">
+            
+                Réserver
+            </li>
+        </a>
+        <a href="info.php" class="navbar-link">
+            <li classe="navbar-item">
+                Infos-pratiques    
+            </li>
+        </a>    
+        <a href="contact.php" class="navbar-link">
+            <li classe="navbar-item">
+                Contact
+            </li>
+        </a>
+    </ul>
+</nav>
+
+<div class="bulle">
+    <div class="logo-container">
+        <a href="index.php">
+            <div class="logo-image">
+                <img classe="logo" src="source/logo.png" alt="logo du site">
+            </div>
+            <div class="logo-title">
+                WoodLand
+            </div>
+        </a>
+    </div>
+</div>
+
