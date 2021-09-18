9ccaac488c7e2dbdc95ee40adf6ea08713c3d473
JulienPirat <julien-021@hotmail.fr>
2021-09-18 20:49:17 +0200

    PageInfo
    Info.css
    info.php

--- info.php
+++ info.php
@@ -1,16 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css" >
-    <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
-    <title>Infos-pratiques</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" >
+  <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
+  <link rel="stylesheet" href="info.css" >
+  <title>Infos-pratiques</title>
 </head>
 <body>
-    <?php 
-        include('navigation.php')
-    ?>
+  <?php
+  include('navigation.php')
+  ?>
+
+  <h1>TARIF</h1>
+  <table>
+    <tr>
+      <td class="TeteTab">Participant</td>
+      <td class="TeteTab">Individuel</td>
+      <td class="TeteTab">Groupe<br/>(dès 10 personnes)</td>
+    </tr>
+    <tr>
+      <td>Adultes (16 ans et +)</td>
+      <td>22€</td>
+      <td>20€</td>
+    </tr>
+    <tr>
+      <td>Juniors (8 à 15 ans)</td>
+      <td>18€</td>
+      <td>16€</td>
+    </tr>
+    <tr>
+      <td>Enfants (5 à 7 ans)</td>
+      <td>13€</td>
+      <td>11€</td>
+    </tr>
+    <tr>
+      <td>Etudiant</td>
+      <td>18€</td>
+      <td>16€</td>
+    </tr>
+  </table>
+
 </body>
 </html>
