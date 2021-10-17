1c70e18f67e7c3194047a2851f86cb0bedb4feb1
JulienPirat <julien-021@hotmail.fr>
2021-10-17 17:41:32 +0200

    réservation
    commencement de l'entete

--- reservation.php
+++ reservation.php
@@ -5,12 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" >
+    <link rel="stylesheet" href="reservation.css" >
     <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'>
     <title>Réservation</title>
 </head>
 <body>
-    <?php 
+    <?php
         include('navigation.php')
     ?>
+
+    <h1>Vous voulez réserver?</h1>
+    <div id="soustitre">
+      <div id="gauche"></div>
+      <h2>Nous avons hâte de vous retrouver chez nous</h2>
+      <div id="droite"/></div>
+    </div>
+
 </body>
 </html>
