5ee1d1214a519291d363c743878626b555e9648e
MathisPct <math712b@gmail.com>
2021-10-25 00:24:20 +0200

    feat: affichage en grille présentation

--- index.php
+++ index.php
@@ -27,6 +27,43 @@
             </div>
         </div>
     </header>
+    <div class="presentation_wrapper">
+        <div class="presentationTitle">
+            <h2>Mais qu'est-ce que WoodLand?</h2>
+        </div>
+        <div class="presentationContainer grid1">
+            <h3>Prêt à relever les défis !</h3>
+            <p>
+            Un domaine de 10 hectares
+            <br>
+            12 parcours coriaces
+            <br>
+            A partir d'1 mètre
+            </p>
+        </div>
+        <div class="presentationContainer grid2">
+            <h3>Trésors de la table ronde</h3>
+            <p>
+                Les chevaliers de la table ronde vous mette à l'épreuve. Finissez les parcours les plus coriaces afin de remporter diverses récompenses grâce à l'aide de nos partenaires.
+            </p>
+        </div>
+        <div class="presentationContainer grid3">
+            <h3>Tarifs attractifs</h3>
+            <p>
+                Tarifs réduits pour nos petits chevaliers
+                <br>
+                Tarifs réduit pour les étudiants
+            </p>
+        </div>
+        <div class="presentationContainer grid4">
+            <h3>Horaires flexibles</h3>
+            <p>
+                Ouvert 6j/7
+                <br>
+                Ouvert la majorité de l'année
+            </p>
+        </div>
+    </div>
     <div class="news_wrapper" id="news">
         <h2>Nos dernières informations</h2>
         <div class="news_container">
@@ -43,7 +80,9 @@
                         soit sur un support imprimé et votre identité (toutes pièces d’identités acceptées ou bien votre nom sur votre réservation est suffisant).
                     </p>
                 </div>
-                <a href ="#" class= "voirBtn"> Voir plus </a>
+                <div class="button-wrapper">
+                    <a href ="#" class= "voirBtn"> Voir plus </a>
+                </div>
             </div>
         </div>
 
@@ -53,13 +92,15 @@
             </div>
             <div class="newsDescription">
                 <div class="textNews">
-                    <h3>Pass sanitaire obligatoire</h3>
+                    <h3>Un nouvel espace de détente!</h3>
                     <p style="text-align: justify">
                         Venez vous détendre entre 2 parcours, dans un merveilleux espace de détente. Nous vous rappelons
                         que les chevaliers ont aussi le droit de se détendre!
                     </p>
                 </div>
-                <a href ="#" class= "voirBtn"> Voir plus </a>
+                <div class="button-wrapper">
+                    <a href ="#" class= "voirBtn"> Voir plus </a>
+                </div>
             </div>
         </div>
     </div>
